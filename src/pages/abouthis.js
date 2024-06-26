cb15e2cade8eb67b3c73a4215c3ce63968313432
WaiWai9999 <zinwaiwainwe9999@gmail.com>
2024-06-26 17:14:20 +0900

    change import

--- src/pages/abouthis.js
+++ src/pages/abouthis.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Link } from "gatsby";
 import Layout from "../components/layout";
-import "../style/common.scss";
 import SEO from "../components/seo";
+import "../style/common.scss";
 
 const abouthis = () => (
   <Layout>
